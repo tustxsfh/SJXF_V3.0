ddc7288e8ce39bb72dbbd31629a692933059d009
tustxsfh <tustxsfh@gmail.com>
2021-04-12 22:09:42 +0800

    +随机学习+听音频

--- peixun_xwj_random.py
+++ peixun_xwj_random.py
@@ -14,9 +14,6 @@ from PIL import Image
 import pytesseract
 from pytesseract import image_to_string
 
-
-
-
 browser = webdriver.Chrome()
 browser.implicitly_wait(60 * 3)
 browser.maximize_window()
@@ -24,7 +21,7 @@ browser.maximize_window()
 start_time = time.time()
 
 
-def login(ume, pwd):    # 登录函数
+def login(ume, pwd):  # 登录函数
 
     # 登录页面
     login_url = "https://www.sxgbxx.gov.cn/login"
@@ -64,15 +61,21 @@ def peixun():
     with open('peixun_url.txt', 'r') as f:
         cou_url_list = f.read().splitlines()
 
-        # sum = len(cou_url_list)
-    for pei_url in cou_url_list:
-        print(pei_url)
+        sum = len(cou_url_list)
+
+    for x in range(1, 50):  # 每次随机学习，学习50次
+
+        # 培训页面
+        pei_url = (cou_url_list[random.randint(1, sum - 1)])
+    # for pei_url in cou_url_list:
         browser.get(pei_url)
+        print('--------------------------------------')
+        print(pei_url)
         # print(browser.page_source)
 
         browser.find_element_by_xpath(
             '//*[@id="aCoursesList"]/div/div[2]/div/div[1]/div/div[2]/div[2]/div/ul/li').click()  # 切换到培训内容详情页
-        print('--------------------------------------')
+
         # print(browser.page_source)
         cou_obj = BeautifulSoup(browser.page_source, 'lxml')
         time.sleep(3)
@@ -83,9 +86,9 @@ def peixun():
         for li in li_list:
 
             end_time = time.time()
-            study_time =(end_time - start_time)/60
-            print('已学习%s分' %study_time)
-            
+            study_time = (end_time - start_time) / 60
+            print('已学习%s分' % study_time)
+
             li_html = str(li)
             # print('--------------------------------------')
             # print(li_html)
@@ -95,7 +98,7 @@ def peixun():
             id = ''.join(id)
             print(id)
 
-            if '视频播放' or '音频' in li_html:
+            if '视频播放' in li_html:
                 title = li.get_text()  # 找到课程标题
                 print(title)
                 shichang = re.findall(r'\d+分\d+秒', li_html)
@@ -153,16 +156,21 @@ def peixun():
                 print('\n')
                 browser.refresh()
 
+
 def keicheng():
     """课程学习"""
     with open('cou_url.txt', 'r') as f:
         cou_url_list = f.read().splitlines()
 
-        # sum = len(cou_url_list)
-    for cou_url in cou_url_list:
-        print(cou_url)
+        sum = len(cou_url_list)
+    for x in range(1, 50):
+
+        # 课程页面
+        cou_url = (cou_url_list[random.randint(1, sum - 1)])
+        # for pei_url in cou_url_list:
         browser.get(cou_url)
-        # print(browser.page_source)
+        print("----------------------------")
+        print(cou_url)
 
         browser.find_element_by_xpath(
             '//*[@id="aCoursesList"]/div/div[2]/div/div[1]/div/div[2]/div[2]/div/ul/li').click()  # 切换到培训内容详情页
@@ -177,8 +185,8 @@ def keicheng():
         for li in li_list:
 
             end_time = time.time()
-            study_time =(end_time - start_time)/60
-            print('已学习%s分' %study_time)
+            study_time = (end_time - start_time) / 60
+            print('已学习%s分' % study_time)
 
             li_html = str(li)
             # print('--------------------------------------')
@@ -189,7 +197,7 @@ def keicheng():
             id = ''.join(id)
             print(id)
 
-            if '视频播放' or '音频' in li_html:
+            if '视频播放' in li_html:
                 title = li.get_text()  # 找到课程标题
                 print(title)
                 shichang = re.findall(r'\d+分\d+秒', li_html)
@@ -236,6 +244,9 @@ def keicheng():
                 browser.refresh()
 
             elif '随堂小测验' in li_html:
+                break
+
+            elif '随堂小测验' in li_html:
                 continue
 
             else:
@@ -252,8 +263,6 @@ def keicheng():
 ume = 'u0283323'
 pwd = 'u0283323'
 login(ume, pwd)
-
-
 
 # 完成课程学习功能
 # keicheng()
